b4ba8d8b2f8b690ede59571be39175f1bc2eb77f
Pluggito <Pluggito@example.com>
2025-01-01 16:47:33 +0100

    let go

--- src/Frontend/assets/asset.js
+++ src/Frontend/assets/asset.js
@@ -1,11 +1,11 @@
 import bg1 from './Background_Image_2-removebg-preview.png'
 import bg2 from './New_Background_Image-removebg-preview.png'
-import bg3 from './aditya-chinchure-ZhQCZjr9fHo-unsplash.jpg'
+
 
 
 export const assets = {
     bg1,
     bg2,
-    bg3
+
 
 }
