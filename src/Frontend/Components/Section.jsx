09fb9686f80d9c52bee243b389aa5f32820b5daa
Pluggito <Pluggito@example.com>
2025-01-02 16:30:53 +0100

    More features added

--- src/Frontend/Components/Section.jsx
+++ src/Frontend/Components/Section.jsx
@@ -1,9 +1,11 @@
-
-
-const Section = () => {
+const Section = ({section}) => {
   return (
-    <div>
-      
+    <div className="item-center flex mt-6">
+      <div className="flex flex-row gap-4 text-center items-center justify-center font-semibold w-full py-5">
+        <hr className="w-32 border-t border-black" />
+        <p className="text-center font-[Poppins] text-3xl">{section}</p>
+        <hr className="w-32 border-t border-black" />
+      </div>
     </div>
   )
 }
