721dfa11b658471b9de1f423fa660f43f4584658
Pluggito <Pluggito@example.com>
2025-01-18 12:53:55 +0100

    Going towards completation

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { HashRouter } from 'react-router'
+import TicketContextProvider from './Frontend/Context/TicketContext.jsx'
 // import { ThemeContext } from './Frontend/Context/ThemeModes.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <HashRouter>
+      <TicketContextProvider>
       <App />
+      </TicketContextProvider>
     </HashRouter>
   </StrictMode>,
 )
